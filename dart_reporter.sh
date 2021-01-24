10f61a5d895cf01c9395cb6877b871e1935cf997
kickbykick <kiibatitoo@gmail.com>
2021-01-24 16:33:48 -0600

    man

--- dart_reporter.sh
+++ dart_reporter.sh
@@ -1,4 +1,5 @@
 flutter pub global activate dart_dot_reporter
+export PATH="$PATH":"/opt/hostedtoolcache/flutter/1.22.5-stable/x64/.pub-cache/bin"
 flutter test --machine > test_info.log || echo 'Tests failed'
 dart_dot_reporter test_info.log > result.log
 echo "hello"
