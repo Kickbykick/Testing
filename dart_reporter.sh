c8e9ee41c53c11c8d77a68a473101866d3ea72bd
kickbykick <kiibatitoo@gmail.com>
2021-01-24 16:57:53 -0600

    binbash

--- dart_reporter.sh
+++ dart_reporter.sh
@@ -1,5 +1,6 @@
-flutter pub global activate dart_dot_reporter
+#!/bin/bash
 export PATH="$PATH":"/opt/hostedtoolcache/flutter/1.22.5-stable/x64/.pub-cache/bin"
+flutter pub global activate dart_dot_reporter
 flutter test --machine > test_info.log || echo 'Tests failed'
 flutter pub global run dart_dot_reporter test_info.log > final_result.log
 echo "hello"
