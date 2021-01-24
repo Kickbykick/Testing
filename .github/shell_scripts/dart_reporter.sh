2b9ae0e1b36c20aceaaab1cb7077376c6665955f
kickbykick <kiibatitoo@gmail.com>
2021-01-24 16:27:22 -0600

    test

--- .github/shell_scripts/dart_reporter.sh
+++ .github/shell_scripts/dart_reporter.sh
@@ -1,4 +1,4 @@
-# flutter pub global activate dart_dot_reporter
-# flutter test --machine > ../../test_info.log || echo 'Tests failed'
+flutter pub global activate dart_dot_reporter
+flutter test --machine > ../../test_info.log || echo 'Tests failed'
 dart_dot_reporter ../../test_info.log > result.log
 echo "hello"
