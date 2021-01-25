5474d5de247335afa9c38eae046a25ffb8b0fec3
kickbykick <kiibatitoo@gmail.com>
2021-01-24 20:11:06 -0600

    more coverage report

--- .github/shell_scripts/cobertura.sh
+++ .github/shell_scripts/cobertura.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 curl -O https://raw.githubusercontent.com/eriwen/lcov-to-cobertura-xml/master/lcov_cobertura/lcov_cobertura.py
-flutter doctor
 flutter test --coverage
 python3 lcov_cobertura.py coverage/lcov.info --output coverage/coverage.xml
--- .github/shell_scripts/dart_reporter.sh
+++ .github/shell_scripts/dart_reporter.sh
@@ -3,4 +3,3 @@ export PATH="$PATH":"/opt/hostedtoolcache/flutter/1.22.5-stable/x64/.pub-cache/b
 flutter pub global activate dart_dot_reporter
 flutter test --machine > test_info.log || echo 'Tests failed'
 flutter pub global run dart_dot_reporter test_info.log
-echo "hello"
