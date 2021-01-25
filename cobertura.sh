3d6e140c2496bf0f72ba5119fea8ddc2931ff3e9
kickbykick <kiibatitoo@gmail.com>
2021-01-24 19:52:56 -0600

    added pushing

--- cobertura.sh
+++ cobertura.sh
@@ -1,5 +1,5 @@
+#!/bin/bash
 curl -O https://raw.githubusercontent.com/eriwen/lcov-to-cobertura-xml/master/lcov_cobertura/lcov_cobertura.py
 flutter doctor
 flutter test --coverage
-dartanalyzer --options analysis_options.yaml
 python3 lcov_cobertura.py coverage/lcov.info --output coverage/coverage.xml
