9e36f912bb8c046a871ddb34ed87f6b491ce4221
kickbykick <kiibatitoo@gmail.com>
2021-01-25 03:52:46 -0600

    kk

--- .github/shell_scripts/cobertura.sh
+++ .github/shell_scripts/cobertura.sh
@@ -2,4 +2,3 @@
 curl -O https://raw.githubusercontent.com/eriwen/lcov-to-cobertura-xml/master/lcov_cobertura/lcov_cobertura.py
 flutter test --coverage
 python3 lcov_cobertura.py coverage/lcov.info --output coverage/coverage.xml
-echo "sick"
