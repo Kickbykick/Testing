37cc16111937fa277f3876b6da6818d9a9cad98d
kickbykick <kiibatitoo@gmail.com>
2021-01-29 12:17:09 -0600

    more

--- .github/shell_scripts/cobertura.sh
+++ .github/shell_scripts/cobertura.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 curl -O https://raw.githubusercontent.com/eriwen/lcov-to-cobertura-xml/master/lcov_cobertura/lcov_cobertura.py
-python3 lcov_cobertura.py coverage/lcov.info --only_changed_files --output coverage/coverage.xml
+python3 lcov_cobertura.py coverage/lcov.info --output coverage/coverage.xml
